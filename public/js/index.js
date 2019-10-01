07f84b7c175a839e0493c5276c69a59c7623a532
Justin <quezada@Justins-MacBook-Pro.local>
2019-09-30 20:57:42 -0400

    added js for bom form

--- public/js/index.js
+++ public/js/index.js
@@ -1,69 +1,41 @@
-// Get references to page elements
-var $author = $("#bom-author");
-var $title = $("bom-title");
-var $submitBtn = $("#submit-btn");
-var $summary = $("#bom-summary");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveExample: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/examples",
-      data: JSON.stringify(example)
-    });
-  },
-  getExamples: function() {
-    return $.ajax({
-      url: "api/examples",
-      type: "GET"
-    });
-  },
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
+$(document).ready(function() {
+  //declaring variables for our inputs
+  var $title = $("input#bom-title");
+  var $author = $("input#bom-author");
+  var $summary = $("input#bom-summary");
+  var $button = $("#submit-btn");
+  //event listner for submit
+  $button.on("click", insertBOM);
+  // inserts a new BOM into the db
+  function insertBOM(event) {
+    event.preventDefault();
+    var bom = {
+      title: $title.val().trim(),
+      author: $author.val().trim(),
+      summary: $summary.val().trim()
+    };
+    $.post("/api/bom", bom);
+    // clear input fields
+    $title.val("");
+    $author.val("");
+    $summary.val("");
   }
-};
+});
 // handleFormSubmit is called whenever we submit a new example
 // Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
+if (!(bom.author && bom.title)) {
+  alert("You must enter the Authors name and Title!");
+  return;
+}
 
-  var bom = {
-    title: $title.val().trim(),
-    author: $author.val().trim(),
-    description: $summary.val().trim()
-  };
+API.saveExample(bom).then(function() {
+  refreshExamples();
+});
 
-  if (!(bom.author && bom.title)) {
-    alert("You must enter the Authors name and Title!");
-    return;
-  }
-
-  API.saveExample(bom).then(function() {
-    refreshExamples();
-  });
-
-  $author.val("");
-  $title.val("");
-};
-
+$author.val("");
+$title.val("");
 // handleDeleteBtnClick is called when an example's delete button is clicked
 // Remove the example from the db and refresh the list
-var handleDeleteBtnClick = function() {
-  var idToDelete = $(this)
-    .parent()
-    .attr("data-id");
-
-  API.deleteExample(idToDelete).then(function() {
-    refreshExamples();
-  });
-};
 
 // Add event listeners to the submit and delete buttons
 $submitBtn.on("click", handleFormSubmit);
